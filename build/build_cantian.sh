613a0d360944eb3528ea2c9947297b43498134a7
liuxinchi <liuxinchi@huawei.com>
2024-02-22 18:46:40 +0800

    4

--- build/build_cantian.sh
+++ build/build_cantian.sh
@@ -17,7 +17,7 @@ source "${CURRENT_PATH}"/common.sh
 
 function packageTarget() {
   echo "Start packageTarget..."
-  cd "${CANTIANDB_BIN}"/"${BUILD_TARGET_NAME}"
+  cd "${CANTIANDB_BIN}"
   tar -zcf cantian.tar.gz ${BUILD_TARGET_NAME}/
   if [ -d /opt/cantian/image ]; then
     rm -rf /opt/cantian/image
